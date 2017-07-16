aebfa7f9e03c7faf6c7c3004ca58ac95c406a859
ankith0073 <ankith0073@gmail.com>
2017-07-16 02:50:00 +0200

    Update tools.cpp

--- src/tools.cpp
+++ src/tools.cpp
@@ -11,8 +11,22 @@ Tools::~Tools() {}
 
 VectorXd Tools::CalculateRMSE(const vector<VectorXd> &estimations,
                               const vector<VectorXd> &ground_truth) {
-  /**
-  TODO:
-    * Calculate the RMSE here.
-  */
+
+    VectorXd rmse(4) ;
+    rmse << 0, 0, 0, 0;
+    for(int i = 0; i < estimations.size(); i++)
+    {
+        //difference
+        VectorXd diff = estimations[i] - ground_truth[i];
+        diff = diff.array() * diff.array();
+        rmse += diff;
+    }
+
+/* check if elements are going above 100, then make them 0 */
+
+    rmse = rmse / estimations.size();
+
+    rmse = rmse.array().sqrt();
+    return rmse;
+
 }
